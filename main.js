52d9c31b7be3c7f0906d8bb0d1bb8098c67a7a4e
yanis.amraoui <yanis.amraoui@gireve.com>
2023-05-21 04:36:40 +0100

    Headless

--- main.js
+++ main.js
@@ -169,7 +169,7 @@ const puppeteerFunction = async () => {
 
     // Lancement du navigateur en mode headless (sans interface graphique) et avec le proxy
     const browser = await puppeteer.launch({
-      headless: false,
+      headless: "new",
       args: [
           `--proxy-server=${proxy+':'+port}`,
           '--no-sandbox',
